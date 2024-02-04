8e432ea083c1d3a43aa6796d92d62243c291d4dd
Charis Trusty <99435146+goodsoop@users.noreply.github.com>
2024-02-04 10:13:08 -0500

    Working program incorporating word_spell

--- main.py
+++ main.py
@@ -1,98 +1,237 @@
 import pygame
 import time
-
+import random
+import cv2
+import pygame_gui
+import sys
 # initialize pygame
 pygame.init()
 
 # screen setup
 screen_width = 800
 screen_height = 600
-screen = pygame.display.set_mode((screen_width, screen_height))
+main_screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("TechTales")
-
+text_font = pygame.font.SysFont("Arial",20)
 # colours for the game (can be changed)
 white = (255, 255, 255)
 black = (0, 0, 0)
 
-# Set up fonts
-font = pygame.font.Font(None, 36)
 
-# matching sounds to letters
-# will need to set up these sounds
-# letter_sounds = {
-#     'a': 'sounds/a_sound.wav',
-#     'b': 'sounds/b_sound.wav',
-#     'c': 'sounds/c_sound.wav',
-#     'd': 'sounds/d_sound.wav',
-#     'e': 'sounds/e_sound.wav',
-#     'f': 'sounds/f_sound.wav',
-#     'g': 'sounds/g_sound.wav',
-#     'h': 'sounds/h_sound.wav',
-#     'i': 'sounds/i_sound.wav',
-#     'j': 'sounds/j_sound.wav',
-#     'k': 'sounds/k_sound.wav',
-#     'l': 'sounds/l_sound.wav',
-#     'm': 'sounds/m_sound.wav',
-#     'n': 'sounds/n_sound.wav',
-#     'o': 'sounds/o_sound.wav',
-#     'p': 'sounds/p_sound.wav',
-#     'q': 'sounds/q_sound.wav',
-#     'r': 'sounds/r_sound.wav',
-#     's': 'sounds/s_sound.wav',
-#     't': 'sounds/t_sound.wav',
-#     'u': 'sounds/u_sound.wav',
-#     'v': 'sounds/v_sound.wav',
-#     'w': 'sounds/w_sound.wav',
-#     'x': 'sounds/x_sound.wav',
-#     'y': 'sounds/y_sound.wav',
-#     'z': 'sounds/z_sound.wav',
-# }
 
-# function to play sounds 
-# need sounds folder to run this
-# def play_sound(letter):
-#     """ this function will play the sound of the letter passed as an argument
 
-#     args:
-#         letter (str): inputted letter to play the sound of
-#     """
-#     sound_path = letter_sounds.get(letter)
-#     if sound_path:
-#         pygame.mixer.Sound(sound_path).play()
+
+
+class Game_Button():
+    def __init__(self, image, coordinates, game_name, font, button_color, highlight_color):
+        self.image = image
+        self.x_pos = coordinates[0]
+        self.y_pos = coordinates[1]
+        self.font = font
+        self.base_color, self.hovering_color = button_color, highlight_color
+        self.text_input = game_name
+        self.text = self.font.render(self.text_input, True, self.base_color)
+        if self.image is None:
+            self.image = self.text
+        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+        self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
+
+    def display_button(self, screen):
+        if self.image is not None:
+            screen.blit(self.image, self.rect)
+        screen.blit(self.text, self.text_rect)
+
+    def check_if_clicked(self, position):
+        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+            return True
+        return False
+
+    def Highlight_button(self, position):
+        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+            self.text = self.font.render(self.text_input, True, self.hovering_color)
+        else:
+            self.text = self.font.render(self.text_input, True, self.base_color)
+
+#this checks if user input is correct or not and shows corresponding image on correctness
+# def Word_Spell_Check(user_input,correct_word):
+#      while True:
+        
+#         for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#                 pygame.quit()
+#                 sys.exit()
+
+#         if user_input == correct_word:
+#             image_choice = pygame.image.load("Correct.png")
+#             image_choice = pygame.transform.scale(image_choice, (100, 100))  # Adjust the size as needed
+#             main_screen.blit(image_choice,(280,400))
+#             pygame.display.update()
+#             pygame.time.delay(600)
+#             return
+#         elif user_input != correct_word:
+#             image_choice = pygame.image.load("Incorrect.png")
+#             image_choice = pygame.transform.scale(image_choice, (100, 100))  # Adjust the size as needed
+#             main_screen.blit(image_choice,(280,400))
+#             pygame.display.update()
+#             pygame.time.delay(600)
+#             return
+       
+
+        
+
+def Word_spell():
+    
+    clock = pygame.time.Clock()
+    
+
+    #gui manager for text input
+    manager = pygame_gui.UIManager((screen_width, screen_height))
+    
+    
+    #Create text input line with cursor
+    text_input = pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((100, 450), (150, 30)), manager=manager, object_id='#main_text_entry')
+    UI_REFRESH_RATE = clock.tick(60)/1000
+
+    #dictionary for possible images
+    image_collection = {"House.png":"house","door.png":"door","Apple.png":"apple"}
+    
+    #choose random picture
+    #chosen_picture,word = random.choice(list(image_collection.items()))
+    #Show given image
+    image_key = random.choice(list(image_collection.keys()))
+    image_choice = pygame.image.load(image_key)
+    image_choice = pygame.transform.scale(image_choice, (300, 300))
+    main_screen.blit(image_choice, (250, 140))
+    pygame.display.update()
+
+    # word corresponding with image
+    word = image_collection[image_key]
+
+    tries = 3
+    while True:
+        
+
+
+        word_spell_mous_pos = pygame.mouse.get_pos()
+
+        main_screen.fill("white")
+
+        Word_spell_text = text_font.render("Spell the name of the given object", True, "Black")
+
+        #Display Game prompt
+        Word_spell_rect = Word_spell_text.get_rect(center=(400, 100))
+        main_screen.blit(Word_spell_text, Word_spell_rect)
+
+        
+        
+
+
+        #Display Quit button
+        Word_Spell_Back_Bttn = Game_Button(image=None, coordinates = (640, 460),game_name="QUIT", font=text_font, button_color="Black", highlight_color="Green")
+        Word_Spell_Back_Bttn.Highlight_button(word_spell_mous_pos)
+        Word_Spell_Back_Bttn.display_button(main_screen)
+
+        
+
+        main_screen.blit(image_choice, (250, 140))
+
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            manager.process_events(event)
+                
+            if (event.type == pygame_gui.UI_TEXT_ENTRY_FINISHED and event.ui_object_id == '#main_text_entry'):
+               if event.text == word:
+                   image_choice = pygame. image.load( "Correct.png") 
+                   image_choice = pygame.transform.scale(image_choice, (100, 100))  # Adjust the size as needed
+                   main_screen.blit(image_choice,(280,400))
+                   pygame.display.update()
+                   pygame.time.delay(1000)
+                   image_key = random.choice(list(image_collection.keys()))
+                   image_choice =  pygame.image.load(image_key)
+                   image_choice =  pygame.transform.scale(image_choice, (300, 300))
+                   main_screen.blit(image_choice, (250, 140))
+                   word =image_collection[image_key]
+                   tries = 3
+               else:
+                   
+                   incorrect = pygame.image.load("Incorrect.png")
+                   incorrect = pygame.transform.scale(incorrect, (100, 100))  # Adjust the size as needed
+                   main_screen.blit(incorrect,(280,400))
+                   pygame.display.update()
+                   pygame.time.delay(1000)
+                   tries -=1
+                   if tries == 0:
+                       
+                       image_key = random.choice(list(image_collection.keys()))
+                       image_choice =  pygame.image.load(image_key)
+                       image_choice =  pygame.transform.scale(image_choice, (300, 300))
+                       main_screen.blit(image_choice, (250, 140))
+                       word = image_collection[image_key]
+                       tries = 3
+                
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if Word_Spell_Back_Bttn.check_if_clicked(word_spell_mous_pos):
+                    main()
+        manager.update(UI_REFRESH_RATE)
+        manager.draw_ui(main_screen)
+            
+        pygame.display.update()
+
+
+
+
+
 
 def main():
     running = True
-    input_text = ""
 
     while running:
+        main_screen.blit(pygame.image.load("Background.png"),(0,0))
+
+        position_of_mouse = pygame.mouse.get_pos()
+
+        menu = text_font.render("Choose a game", True, "#b68f40")
+        menu_rectangle = menu.get_rect(center=(400,100))
+
+        #push text onto main screen
+        
+
+
+        # Please create your game buttons using the this format
+        # game_name_here = Game_Button(pygame.image.load("your_button_icon"),(x,y),"The name of your game here",text_font,"Blue","Green")
+        Word_Spell_button = Game_Button(pygame.image.load("button.png"),(200,200),"Word Spell",text_font,"Blue","Green")
+
+        main_screen.blit(menu,menu_rectangle)
+
+        #This displays button on screen
+        Word_Spell_button.display_button(main_screen)
+
+        #This highlights the button text when the mouse hovers over button
+        Word_Spell_button.Highlight_button(position_of_mouse)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN:
-                    # after the enter key is pressed, play the sound for the entered word
-                    for letter in input_text:
-                        play_sound(letter)
-                        time.sleep(0.5)  # add a short delay between sounds
-                    input_text = ""
-                elif event.key == pygame.K_BACKSPACE:
-                    # case for backspace key
-                    input_text = input_text[:-1]
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                #create all if statements for function/game calls
+                if Word_Spell_button.check_if_clicked(position_of_mouse):
+                    Word_spell() # this can be erased and replaced with function call
+                    
                 else:
-                    # add pressed key to the input text
-                    input_text += event.unicode
-
-        # clear screen
-        screen.fill(white)
-
-        # display the input text on the screen
-        text = font.render(input_text, True, black)
-        screen.blit(text, (10, 10))
+                    break
+                    # put name of game here
 
         # update the display
-        pygame.display.flip()
+        pygame.display.update()
 
     pygame.quit()
 
 if __name__ == "__main__":
     main()
+
+
